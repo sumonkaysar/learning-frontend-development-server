4a7bc8fdf348f4f4c301e1c125da0a728204b1c8
Sumon Kaysar <msk.sumon.kaysar@gmail.com>
2022-10-25 21:40:57 +0600

    deployed to vercel

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
-const port = 5000;
 const courses = require('./data/courses.json')
 
+const app = express()
+const port = process.env.PORT ||5000;
 
 app.use(cors())
 
