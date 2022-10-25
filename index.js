05570e3fbdfe0de0b66225d1d29ced9e4c184832
Sumon Kaysar <msk.sumon.kaysar@gmail.com>
2022-10-25 20:52:08 +0600

    added cors in index

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 5000;
+
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Learning frontend development server running')
 })
+
 
 app.listen(port, () => {
   console.log(`Iphone server is running on port: ${port}`)
